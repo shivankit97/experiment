8ab7ff4127743511e272b7af3f92563d219c42f9
shivankit97 <shivankitr.97@gmail.com>
2017-09-18 17:59:16 +0530

    new feature completed

--- abc.js
+++ abc.js
@@ -2,6 +2,8 @@ function newfeature(){
     console.log('lines of code');
         console.log('lines of code');
     console.log('half way done with new feature');
+        console.log('At last, done with new feature');
+
 }
 console.log('hello world!');
 function something(){
