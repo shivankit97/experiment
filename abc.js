15456f4bea64bc104c0cdbfa502d2cfa24f49527
shivankit97 <shivankitr.97@gmail.com>
2017-09-18 17:49:57 +0530

    half way done with the new feature coding

--- abc.js
+++ abc.js
@@ -1,5 +1,11 @@
+function newfeature(){
+    console.log('lines of code');
+        console.log('lines of code');
+    console.log('half way done with new feature');
+}
 console.log('hello world!');
 function something(){
     console.log('something to print');
 }
 something();
+newfeature();
